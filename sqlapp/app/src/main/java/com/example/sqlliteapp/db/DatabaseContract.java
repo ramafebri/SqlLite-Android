85cb5beeecde37cc45ed10439c447b2399287381
rama febriansyah <rama_febri48@yahoo.com>
2021-01-06 21:09:48 +0700

    Add sql lite program

---
+++ sqlapp/app/src/main/java/com/example/sqlliteapp/db/DatabaseContract.java
@@ -0,0 +1,17 @@
+package com.example.sqlliteapp.db;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+    public static final class NoteColumns implements BaseColumns {
+        public static final String TABLE_NAME = "note";
+
+        //Note title
+        public static final String TITLE = "title";
+        //Note description
+        public static final String DESCRIPTION = "description";
+        //Note date
+        public static final String DATE = "date";
+
+    }
+}
